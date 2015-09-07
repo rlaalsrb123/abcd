e6907b2ce51f3a58876fcd775a907d3f72e8d188
rlaalsrb123 <darling92@nate.com>
2015-09-07 11:04:55 +0900

    변수정의1

--- app/src/main/java/com/example/kim/abcd/MainActivity.java
+++ app/src/main/java/com/example/kim/abcd/MainActivity.java
@@ -6,7 +6,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
-int a;
+int abc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
